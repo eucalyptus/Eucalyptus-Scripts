71232cd19d89239107f2e4bf9794e3d821af57d2
Graziano Obertelli <graziano@eucalyptus.com>
2011-07-13 11:09:26 -0700

    Made the planet directory a variable
    The directory where planet is configured is now a variable on top of the
    file for easy configuration.

--- PlanetRecipe/planet.sh
+++ PlanetRecipe/planet.sh
@@ -11,6 +11,9 @@ WALRUS_URL="http://173.205.188.8:8773/services/Walrus/planet/"
 PLANET_USER="www-data"
 PLANET_GROUP="www-data"
 
+# where is planet
+WHERE="${WHERE}"
+
 # update the instance
 aptitude -y update
 aptitude -y upgrade
@@ -19,34 +22,34 @@ aptitude -y upgrade
 aptitude install -y planet-venus nginx
 
 # create planet's structure
-mkdir -pv /mnt/planet/cache /mnt/planet/output /mnt/planet/output/images /mnt/planet/theme /mnt/planet/theme/images
-echo "<html></html>" >/mnt/planet/output/index.html
-cp -pv /usr/share/planet-venus/theme/common/* /mnt/planet/theme
-cp -pvr /usr/share/planet-venus/theme/default/* /mnt/planet/theme
-cp -pv /usr/share/planet-venus/theme/common/images/* /mnt/planet/output/images
+mkdir -pv ${WHERE}/cache ${WHERE}/output ${WHERE}/output/images ${WHERE}/theme ${WHERE}/theme/images
+echo "<html></html>" >${WHERE}/output/index.html
+cp -pv /usr/share/planet-venus/theme/common/* ${WHERE}/theme
+cp -pvr /usr/share/planet-venus/theme/default/* ${WHERE}/theme
+cp -pv /usr/share/planet-venus/theme/common/images/* ${WHERE}/output/images
 
 # let's create a script to update the skinning of the planet
-cat >/mnt/planet/execute <<EOF
+cat >${WHERE}/execute <<EOF
 #!/bin/sh
-curl -f -o /mnt/planet/planet.ini --url $WALRUS_URL/planet.ini
-curl -f -o /mnt/planet/theme/index.html.tmpl --url $WALRUS_URL/index.html.tmpl
-curl -f -o /mnt/planet/output/images/logo.png --url $WALRUS_URL/logo.png
-curl -f -o /mnt/planet/output/planet.css --url $WALRUS_URL/planet.css
-cd /mnt/planet && planet --verbose planet.ini
+curl -f -o ${WHERE}/planet.ini --url $WALRUS_URL/planet.ini
+curl -f -o ${WHERE}/theme/index.html.tmpl --url $WALRUS_URL/index.html.tmpl
+curl -f -o ${WHERE}/output/images/logo.png --url $WALRUS_URL/logo.png
+curl -f -o ${WHERE}/output/planet.css --url $WALRUS_URL/planet.css
+cd ${WHERE} && planet --verbose planet.ini
 EOF
 
 # let's run it now
-chmod +x /mnt/planet/execute
-/mnt/planet/execute
+chmod +x ${WHERE}/execute
+${WHERE}/execute
 
 # and turn it into a cronjob
-cat >/mnt/planet/crontab <<EOF
-2,17,32,47 * * * * /mnt/planet/execute
+cat >${WHERE}/crontab <<EOF
+2,17,32,47 * * * * ${WHERE}/execute
 EOF
 
 # change permissions and then start the cronjob
-chown -R ${PLANET_USER}:${PLANET_GROUP} /mnt/planet
-crontab -u ${PLANET_USER} /mnt/planet/crontab
+chown -R ${PLANET_USER}:${PLANET_GROUP} ${WHERE}
+crontab -u ${PLANET_USER} ${WHERE}/crontab
 
 # let's remove the link to the default website
 rm /etc/nginx/sites-available/default
@@ -58,7 +61,7 @@ server {
 	listen   [::]:80 default ipv6only=on; ## listen for ipv6
 	access_log /var/log/nginx/access.log;
 	location / {
-		root	/mnt/planet/output;
+		root	${WHERE}/output;
 		index	index.html;
 	}
 }
