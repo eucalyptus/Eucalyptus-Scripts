d8ea1db8cfc368fbde61d85b70a9d15f9b8e322c
Graziano Obertelli <graziano@eucalyptus.com>
2011-08-12 13:33:36 -0700

    Few permissions issues, typo and small stuff
    The previous script failed to start properly the cronjob. Fixed it.
    Permissions and other small things got fixed too.

--- PlanetRecipe/planet.sh
+++ PlanetRecipe/planet.sh
@@ -31,28 +31,30 @@ cp -pv /usr/share/planet-venus/theme/common/images/* ${WHERE}/output/images
 # let's create a script to update the skinning of the planet
 cat >${WHERE}/execute <<EOF
 #!/bin/sh
-curl -f -o ${WHERE}/planet.ini --url $WALRUS_URL/planet.ini
-curl -f -o ${WHERE}/theme/index.html.tmpl --url $WALRUS_URL/index.html.tmpl
-curl -f -o ${WHERE}/output/images/logo.png --url $WALRUS_URL/logo.png
-curl -f -o ${WHERE}/output/planet.css --url $WALRUS_URL/planet.css
+curl -f -o ${WHERE}/planet.ini --url ${WALRUS_URL}/planet.ini
+curl -f -o ${WHERE}/theme/index.html.tmpl --url ${WALRUS_URL}/index.html.tmpl
+curl -f -o ${WHERE}/output/images/logo.png --url ${WALRUS_URL}/logo.png
+curl -f -o ${WHERE}/output/planet.css --url ${WALRUS_URL}/planet.css
 cd ${WHERE} && planet --verbose planet.ini
 EOF
 
-# let's run it now
+# change execut permission
 chmod +x ${WHERE}/execute
-${WHERE}/execute
 
 # and turn it into a cronjob
 cat >${WHERE}/crontab <<EOF
 2,17,32,47 * * * * ${WHERE}/execute
 EOF
 
-# change permissions and then start the cronjob
-chown -R ${PLANET_USER}:${PLANET_GROUP} ${WHERE}
+# change the ownership of all planet stuff
+chown -R ${PLANET_USER}.${PLANET_GROUP} ${WHERE}
+su -c "${WHERE}/execute" - ${PLANET_USER}
+
+# and start the cronjob
 crontab -u ${PLANET_USER} ${WHERE}/crontab
 
 # let's remove the link to the default website
-rm /etc/nginx/sites-available/default
+rm /etc/nginx/sites-enabled/default
 
 # let's create our own simple configuration
 cat >/etc/nginx/sites-available/eucalyptus <<EOF
@@ -71,5 +73,5 @@ EOF
 ln -s /etc/nginx/sites-available/eucalyptus /etc/nginx/sites-enabled/eucalyptus
 
 # start the service
-service nginx start
+/etc/init.d/nginx restart
 
