b04bd118d0cfcc9e40dafa10fa1be9f1c78b0d6e
Graziano Obertelli <graziano@eucalyptus.com>
2011-10-18 16:23:13 -0700

    First cut at the script
    Few changes:
    - curl is now silent
    - exim4 wanted also update*conf to be changed (we create it on the fly)
    - exim4 seems to have an issue which prevent it to be stopped (kill will
      do)
    - the local IP is used instead of 127.0.0.1 for the MAILNAME
    - check returned files from walrus for errors

--- MailmanRecipe/mailman.sh
+++ MailmanRecipe/mailman.sh
@@ -14,7 +14,6 @@ WALRUS_MASTER="mailman-archive.tgz"	# master copy of the database
 # mailman related configuration
 MAILNAME="lists.eucalyptus.com"         # the public hostname
 POSTMASTER="community@eucalyptus.com"   # email to receive exim errors
-PASSWD="pippo"                          # password for administer lists
 MOUNT_POINT="/mailman"                  # archives and data are on ephemeral
 
 # do backup on walrus?
@@ -28,7 +27,7 @@ S3CURL="/usr/bin/s3curl-euca.pl"
 
 # get the s3curl script
 echo "Getting ${S3CURL}"
-curl -f -o ${S3CURL} --url http://173.205.188.8:8773/services/Walrus/s3curl/s3curl-euca.pl
+curl -s -f -o ${S3CURL} --url http://173.205.188.8:8773/services/Walrus/s3curl/s3curl-euca.pl
 chmod 755 ${S3CURL}
 
 # now let's setup the id for accessing walrus
@@ -50,14 +49,18 @@ apt-get --force-yes -y update
 apt-get --force-yes -y upgrade
 
 # make sure the mailname is correct
+echo "Setting hostname and mailname to ${MAILNAME}"
 echo "${MAILNAME}" > /etc/mailname
+echo "${MAILNAME}" > /etc/hostname
+hostname ${MAILNAME}
+LOCALIP="`curl -s -f -m 20 http://169.254.169.254/latest/meta-data/local-ipv4`"
+echo -e "${LOCALIP}\t${MAILNAME}" >> /etc/hosts
 
 # mailman and exim requires some preseed to prevent questions
+echo "Preseeding debconf for mailman and exim"
 cat >/root/preseed.cfg <<EOF
-exim4-daemon-light      exim4-daemon-light/drec error   
 exim4-config    exim4/dc_other_hostnames        string  
-exim4-config    exim4/dc_eximconfig_configtype  select  internet site;
-mail is sent and received directly using SMTP
+exim4-config    exim4/dc_eximconfig_configtype  select  internet site; mail is sent and received directly using SMTP
 exim4-config    exim4/no_config boolean true
 exim4-config    exim4/hide_mailname     boolean 
 exim4-config    exim4/dc_postmaster     string  ${POSTMASTER}
@@ -68,7 +71,6 @@ exim4-base      exim4/purge_spool       boolean false
 exim4-config    exim4/mailname  string  ${MAILNAME}
 exim4-config    exim4/dc_readhost       string  
 # Reconfigure exim4-config instead of this package
-exim4-base      exim4-base/drec error   
 exim4-config    exim4/use_split_config  boolean false
 exim4-config    exim4/dc_localdelivery  select  mbox format in /var/mail/
 exim4-config    exim4/dc_local_interfaces       string  
@@ -85,20 +87,21 @@ debconf-set-selections /root/preseed.cfg
 rm -f /root/preseed.cfg
 
 # install mailman
+echo "Installing mailman"
 apt-get install --force-yes -y mailman
 
 # let's make sure we have the mountpoint
-echo "Creating and prepping $MOUNT_POINT"
-mkdir -p $MOUNT_POINT
+echo "Creating and prepping ${MOUNT_POINT}"
+mkdir -p ${MOUNT_POINT}
 
-# don't mount $MOUNT_POINT more than once (mainly for debugging)
-if ! mount |grep $MOUNT_POINT; then
+# don't mount ${MOUNT_POINT} more than once (mainly for debugging)
+if ! mount |grep ${MOUNT_POINT}; then
         # let's see where ephemeral is mounted, and either mount
-        # it in the final place ($MOUNT_POINT) or mount -o bind
-        EPHEMERAL="`curl -f -m 20 http://169.254.169.254/latest/meta-data/block-device-mapping/ephemeral0`"
+        # it in the final place (${MOUNT_POINT}) or mount -o bind
+        EPHEMERAL="`curl -s -f -m 20 http://169.254.169.254/latest/meta-data/block-device-mapping/ephemeral0`"
         if [ -z "${EPHEMERAL}" ]; then
                 # workaround for a bug in EEE 2
-                EPHEMERAL="`curl -f -m 20 http://169.254.169.254/latest/meta-data/block-device-mapping/ephemeral`"
+                EPHEMERAL="`curl -s -f -m 20 http://169.254.169.254/latest/meta-data/block-device-mapping/ephemeral`"
         fi
         if [ -z "${EPHEMERAL}" ]; then
                 echo "Cannot find ephemeral partition!"
@@ -106,38 +109,77 @@ if ! mount |grep $MOUNT_POINT; then
         else
                 # let's see if it is mounted
                 if ! mount | grep ${EPHEMERAL} ; then
-                        mount /dev/${EPHEMERAL} $MOUNT_POINT
+                        mount /dev/${EPHEMERAL} ${MOUNT_POINT}
                 else
-                        mount -o bind `mount | grep ${EPHEMERAL} | cut -f 3 -d ' '` $MOUNT_POINT
+                        mount -o bind `mount | grep ${EPHEMERAL} | cut -f 3 -d ' '` ${MOUNT_POINT}
                 fi
         fi
 fi
 
+# now let's get the exim configured
+echo "Creating exim4 configuration"
+cat >/etc/exim4/update-exim4.conf.conf <<EOF
+# This is a Debian specific file
+
+dc_eximconfig_configtype='internet'
+dc_other_hostnames=''
+dc_local_interfaces=''
+dc_readhost=''
+dc_relay_domains=''
+dc_minimaldns='false'
+dc_relay_nets=''
+dc_smarthost=''
+CFILEMODE='644'
+dc_use_split_config='false'
+dc_hide_mailname=''
+dc_mailname_in_oh='true'
+dc_localdelivery='mail_spool'
+EOF
+
+# regenerate config and restart service
+service exim4 stop
+# there seems to be a bug for which exim is not properly stopped
+killall exim4
+update-exim4.conf
+service exim4 start
+
 # now let's get the archives from the walrus bucket
-${S3CURL} --id ${WALRUS_NAME} -- ${WALRUS_URL}/${WALRUS_MASTER} > /$MOUNT_POINT/master_copy.tgz
-mkdir /$MOUNT_POINT/mailman
-tar -C /$MOUNT_POINT/mailman -xzf /$MOUNT_POINT/master_copy.tgz
-mv /var/lib/mailman /var/lib/mailman.orig
-ln -s /$MOUNT_POINT/mailman /var/lib/mailman
+echo "Retrieving mailman archives and configuration"
+${S3CURL} --id ${WALRUS_NAME} -- -s ${WALRUS_URL}/${WALRUS_MASTER} > /${MOUNT_POINT}/master_copy.tgz
+mkdir /${MOUNT_POINT}/mailman
+if [ "`head -c 4 /${MOUNT_POINT}/master_copy.tgz`" = "<Err" ]; then
+        echo "Couldn't get archives!"
+        exit 1
+else
+        tar -C /${MOUNT_POINT}/mailman -xzf /${MOUNT_POINT}/master_copy.tgz
+        mv /var/lib/mailman /var/lib/mailman.orig
+        ln -s /${MOUNT_POINT}/mailman /var/lib/mailman
+fi
 
 # and the aliases
-${S3CURL} --id ${WALRUS_NAME} -- ${WALRUS_URL}/aliases > /etc/aliases
-newaliases
-
+${S3CURL} --id ${WALRUS_NAME} -- -s ${WALRUS_URL}/aliases > /${MOUNT_POINT}/aliases
+if [ "`head -c 4 /${MOUNT_POINT}/aliases`" = "<Err" ]; then
+        echo "Couldn't get aliases!"
+        exit 1
+else
+        mv /${MOUNT_POINT}/aliases /etc/aliases
+        newaliases
+fi
 
 # set up a cron-job to save the archives and config to a bucket: it will
 # run as root
+echo "Preparing local script to push backups to warlrus"
 cat >/usr/local/bin/mailman_backup.sh <<EOF
 #!/bin/sh
-tar -C /var/lib/mailman -czf /$MOUNT_POINT/archive.tgz . 
+tar -C /var/lib/mailman -czf /${MOUNT_POINT}/archive.tgz . 
 # WARNING: the bucket in ${WALRUS_URL} *must* have been already created
 # keep one copy per day of the month
-${S3CURL} --id ${WALRUS_NAME} --put /$MOUNT_POINT/archive.tgz -- ${WALRUS_URL}/${WALRUS_MASTER}-day_of_month
+${S3CURL} --id ${WALRUS_NAME} --put /${MOUNT_POINT}/archive.tgz -- -s ${WALRUS_URL}/${WALRUS_MASTER}-day_of_month
 # and push it to be the latest backup too for easy recovery
-${S3CURL} --id ${WALRUS_NAME} --put /$MOUNT_POINT/archive.tgz -- ${WALRUS_URL}/${WALRUS_MASTER}
+${S3CURL} --id ${WALRUS_NAME} --put /${MOUNT_POINT}/archive.tgz -- -s ${WALRUS_URL}/${WALRUS_MASTER}
 # and save the aliases too
-${S3CURL} --id ${WALRUS_NAME} --put /etc/aliases -- ${WALRUS_URL}/aliases
-rm /$MOUNT_POINT/archive.tgz
+${S3CURL} --id ${WALRUS_NAME} --put /etc/aliases -- -s ${WALRUS_URL}/aliases
+rm /${MOUNT_POINT}/archive.tgz
 EOF
 # substitute to get the day of month
 sed -i 's/-day_of_month/-$(date +%d)/' /usr/local/bin/mailman_backup.sh
@@ -151,6 +193,7 @@ if [ "$WALRUS_BACKUP" != "Y" ]; then
 fi
 
 # and turn it into a cronjob to run every hour
+echo "Setting up cron-job"
 cat >/tmp/crontab <<EOF
 30 * * * * /usr/local/bin/mailman_backup.sh
 EOF
